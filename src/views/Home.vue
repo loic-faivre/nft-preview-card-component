f79bb010352833dbac0d4cb3e70f0baf1a5eea6c
Loic <loicf69@yahoo.fr>
2021-12-29 22:44:58 +0100

    All design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="card">
+    <div class="img">
+      <img class="image" src="../assets/images/image-equilibrium.jpg">
+      <div class="overlay">
+        <img class="view" src="../assets/images/icon-view.svg">
+      </div>
+    </div>
+    <div class="content">
+      <h3 class="title">Equilibrium #3429</h3>
+      <p>Our Equilibrium collection promotes balance and calm.</p>
+      <div class="info">
+        <span class="price"><img src="../assets/images/icon-ethereum.svg"/> 0.041 ETH</span>
+        <span class="time"><img src="../assets/images/icon-clock.svg" /> 3 days left</span>
+      </div>
+    </div>
+    <div class="footer">
+      <img class="avatar" src="../assets/images/image-avatar.png">
+      Creation of <span>Jules Wyvern</span>
+    </div>
+    <div class="sign">
+      <a href="https://github.com/loic-faivre">Lolof</a>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
 };
 </script>
